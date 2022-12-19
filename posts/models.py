b49dac1fa8c823273b3cb0e20b5a315a695a2712
vinicius <viniciuseugeniovhe@gmail.com>
2022-12-19 15:24:39 -0300

    Redimensionamento de imagem incrementado

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from categorias.models import Categoria
+from PIL import Image
+from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -16,3 +18,28 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem:
+            self.resize_image(self.imagem.name, 800)
+        
+    @staticmethod
+    def resize_image(img_name, new_width):
+        img_path = settings.MEDIA_ROOT / img_name
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = (new_width * height) / width
+
+        if width <= new_width:
+            img.close()
+            return
+
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            img_path,
+            optimize=True,
+            quality=60,
+        )
+        new_img.close()
